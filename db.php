04bd68891008aa8ed80dcfac7b1e5216ad1056d9
melvenko <54460468+melvenko@users.noreply.github.com>
2021-12-13 14:47:32 +0800

    adding login script

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Author: Javed Ur Rehman
+Website: http://www.allphptricks.com/
+*/
+
+
+$con = mysqli_connect("db684486805.db.1and1.com","dbo684486805","Inf0rm@ti0nRequestForm","db684486805");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
