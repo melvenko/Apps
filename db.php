50ffd78ebcb48e92d39daa03b221399557563220
melvenko <54460468+melvenko@users.noreply.github.com>
2021-12-13 15:00:58 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("db5006028797.hosting-data.io","dbu1537526","myNewPassword@123"," dbs5049760");
+$con = mysqli_connect("db5006028797.hosting-data.io","dbu1537526","myNewPassword@123","dbs5049760");
 // Check connection
 if (mysqli_connect_errno())
   {
