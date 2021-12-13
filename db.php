84f30e77b2b266de829cf0f9dbfbda8ea3692494
melvenko <54460468+melvenko@users.noreply.github.com>
2021-12-13 14:49:21 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,4 @@
 <?php
-/*
-Author: Javed Ur Rehman
-Website: http://www.allphptricks.com/
-*/
-
 
 $con = mysqli_connect("db684486805.db.1and1.com","dbo684486805","Inf0rm@ti0nRequestForm","db684486805");
 // Check connection
