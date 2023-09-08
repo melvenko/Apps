b304790e1cfef3896480feca6e5464999913bd40
melvenko <54460468+melvenko@users.noreply.github.com>
2023-09-08 20:37:56 +0800

    Update index.php
    adding redirection to login.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
+<?php header("Location: https://login.melvenko.com/login.php"); ?>
 <html>
 <body>
 <center>
   <h2>ATAM Lang MALAKAS</h2>
-  <?php header("Location: https://login.melvenko.com/login.php"); ?>
   <p>test</p>
   <img src ="https://login.melvenko.com/logo.png"></center>
 </body>
