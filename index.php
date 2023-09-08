6d1acaccd75a48f11f26a25a3241e4a5ab5c1b9c
melvenko <54460468+melvenko@users.noreply.github.com>
2023-09-08 20:31:50 +0800

    Update index.php
    logo

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
   <h2>ATAM Lang MALAKAS</h2>
   <?php header("Location: https://login.melvenko.com/login.php"); ?>
   <p>test</p>
-  <img src ="http://cloud.melvenko.com/mywebsite.jpg"></center>
+  <img src ="https://login.melvenko.com/logo.png"></center>
 </body>
 </html>
