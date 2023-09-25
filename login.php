132b541c96643c6bf260b2578c83fb0e51143f68
melvenko <54460468+melvenko@users.noreply.github.com>
2023-09-26 01:57:58 +0800

    Update login.php
    update consent script

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<script type="text/javascript" crossorigin src='//consent.truste.com/notice?domain=melventest.com&c=teconsent'></script>
+<script type="text/javascript" crossorigin src='//consent.trustarc.com/notice?domain=melventest.com&c=teconsent'></script>
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
