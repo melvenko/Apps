d156811618c02d64d312f36fbcc12f9354d738b5
melvenko <54460468+melvenko@users.noreply.github.com>
2023-09-25 17:57:57 +0800

    Update login.php
    re-arranged with divs

--- login.php
+++ login.php
@@ -12,6 +12,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <script src=//consent.trustarc.com/notice?domain=melventest.com&c=teconsent&js=bb&noticeType=bb&gtm=1></script>
 	
 <style>
+
+.indent-1 {float: left;}
+.indent-1 section {width: 50%; float: left;}
+	
 .truste_caIcon_display
 {
         font-size: small;
@@ -32,15 +36,16 @@ img.truste_border_none.truste_cursor_pointer{
 <link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	
+<div id="container">
+	<div id="header">
+		<div class="nav"></div>
+		<div class="logo"><img src="/logo.png" style="width:251px;height:39.5px;"></div>
+	</div>
+	<div id="main">
 <?php
 
 ?>
-<!--  Google Tag Manager (noscript) -->
-<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQLLZBP3"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
-<!-- End Google Tag Manager (noscript) -->
-	
+
 <?php
 	require('db.php');
 	session_start();
@@ -64,38 +69,55 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 				}
     }else{
 ?>
-<img src="/logo.png" style="width:251px;height:39.5px;">
-<div class="form">
-<h1>Log In</h1>
-<form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Username" required />
-<input type="password" name="password" placeholder="Password" required />
-<input name="submit" type="submit" value="Login" />
-</form>
-<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
-<p><div id="teconsent" style="display: none;"></div></p>
 
+	    <section class="indent-1">
+    		<!-- Section 1 --> 
+    		<section>
+        	    <div class="form">
+			<h1>Log In</h1>
+			<form action="" method="post" name="login">
+			<input type="text" name="username" placeholder="Username" required />
+			<input type="password" name="password" placeholder="Password" required />
+			<input name="submit" type="submit" value="Login" />
+			</form>
+			<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
+			<p><div id="teconsent" style="display: none;"></div></p>
+		    </div>
+        		<div>Some more 1</div>
+    		</section>
+
+    		<!-- Section 2 -->
+    		<section>	
+        		<div id="YTvideo" class="reg-elements">
+			    <iframe width="560" height="315" src="https://www.youtube.com/embed/Lffeb73sCx4?si=-POMG6FvYqrxIEmS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			</div>
+        		<center><div id="google-ads">
+			  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1426934089069316"
+     crossorigin="anonymous"></script>
+		<!-- php login -->
+		<ins class="adsbygoogle"
+     		style="display:block"
+     		data-ad-client="ca-pub-1426934089069316"
+     		data-ad-slot="1774009499"
+     		data-ad-format="auto"
+     		data-full-width-responsive="true"></ins>
+	<script>
+     		(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+			</div></center>
+    		</section>
+	    </section>  
+
+<!--- SEPARATOR--->
+		
 
 <?php } 
 ?>
-<br><div id="consent_blackbar"></div> 
-
-<div id="YTvideo" class="reg-elements">
-	<iframe width="560" height="315" src="https://www.youtube.com/embed/Lffeb73sCx4?si=-POMG6FvYqrxIEmS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-</div>
-<center><div id="google-ads">
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1426934089069316"
-     crossorigin="anonymous"></script>
-<!-- php login -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1426934089069316"
-     data-ad-slot="1774009499"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div></center>
+		<div id="widget1">
+	
+	</div>
+	<div id="footer">
+		<div id="consent_blackbar"></div> 
+	</div>
 </body>
 </html>
